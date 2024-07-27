a7ea0d7e1c84a28ff848671a3a2a35d307792fb1
Justin <j.hebenstreit25@gmail.com>
2024-07-27 15:02:09 -0500

    added code for user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,65 @@
 // TODO: Include packages needed for this application
 import fs from 'fs'
 import inquirer from 'inquirer'
-// TODO: Create an array of questions for user input
-const readmeQuestions = [];
 
+// TODO: Create an array of questions for user input
+// const readmeQuestions = [];
+
+inquirer.prompt ([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectName'
+    },
+    {
+       type: 'input',
+       message: 'What is the what, why, and how description of your project?',
+       name: 'projectDescription'
+    },
+    {
+        type: 'input',
+        message: 'What are the the step-by-step instructions on how someone can install your project?',
+        name: 'installationInstructions'
+    },
+    {
+        type: 'input',
+        message: 'How can someone else use your project?',
+        name: 'usageInstructions'
+    },
+    {
+        type: 'input',
+        message: 'What resources were used or who contributed to the project?',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'What tests, commands, or resources were used to test your project?',
+        name: 'testInstructions'
+    },
+    {
+        type: 'list',
+        message: 'Which license was used on your project?',
+        name: 'licenseInformation'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub name?',
+        name: 'gitHub'
+    },
+    {
+        type: 'input',
+        message: 'What email address can you be reached at?',
+        name: 'emailAddress'
+    },
+])
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// .then((answers) => console.log(answers))
+// function writeToFile(fileName, data) {}
+
+.then((answers) => {
+    
+})
 
 // TODO: Create a function to initialize app
 function init() {}
