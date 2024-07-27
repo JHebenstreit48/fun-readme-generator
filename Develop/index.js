cc92e990cd759a59383d2b9b19af43eeb0b6aaef
Justin <j.hebenstreit25@gmail.com>
2024-07-27 12:24:51 -0500

    added some code to the starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-
+import fs from 'fs'
+import inquirer from 'inquirer'
 // TODO: Create an array of questions for user input
-const questions = [];
+const readmeQuestions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
