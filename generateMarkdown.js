04a1426137cdedb424382b724de28797ceed3b45
Justin <j.hebenstreit25@gmail.com>
2024-07-29 15:39:56 -0500

    finished up edits on this project and tested it

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,41 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenseInformation) {
+  switch(licenseInformation) {
+    case 'MIT':
+      return '![Static Badge](https://img.shields.io/badge/License-MIT-yellow)';
+    
+      case 'GNU GPLv3':
+        return '![Static Badge](https://img.shields.io/badge/License-GNU%20GPLv3-blue)';
+
+      case 'Apache License 2.0':
+      return '![Static Badge](https://img.shields.io/badge/License-Apache%20License%202.0-green)';
+
+      case 'ISC':
+      return '![Static Badge](https://img.shields.io/badge/License-ISC-darkred)';
+
+      case 'none':
+        return '';
+
+      default:
+        return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseInformation) {
+  switch(licenseInformation) {
+    case 'MIT':
+      return 'https://opensource.org/license/MIT';
+    
+      case 'GNU GPLv3':
+        return 'https://www.gnu.org/licenses/gpl-3.0';
+
+      case 'Apache License 2.0':
+      return 'https://opensource.org/license/apache-2-0';
+
+      case 'ISC':
+      return 'https://opensource.org/license/isc-license-txt';
+
+      case 'none':
+        return '';
+
+      default:
+        return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseInformation) {
+  if (!licenseInformation || licenseInformation === 'none') {
+    return '';
+  }
+  return `## License
+  This project is licensed under the [${licenseInformation}](${renderLicenseLink(licenseInformation)}) license.`
+  
+}
 
 // TODO: Create a function to generate markdown for README
 
-
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.projectName}
 
-`;
-}
+  ${renderLicenseBadge(data.licenseInformation)}
 
-export default generateMarkdown;
+  ## Description
 
+  ${data.projectDescription}
 
-const generateMarkdown = ({projectName, projectDescription, installationInstructions, usageInstructions, contributions, testInstructions, licenseInformation, gitHub, emailAddress}) => `# <Your-Project-Title>
-
-## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+  ## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
@@ -44,47 +78,27 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${data.installationInstructions}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+${data.usageInstructions}
 
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+${renderLicenseSection(data.licenseInformation)}
 
-    md
-    ![alt text](assets/images/screenshot.png)
+## Contribution Instructions
 
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${data.contributions}
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+${data.testInstructions}
+  
+## Questions
+
+For any further questions you can contact me at [${data.emailAddress}](mailto:${data.emailAddress}). You can find projects I have created or participated in at [${data.gitHub}](https://github.com/${data.gitHub}).
+
+`;
+}
+
+export default generateMarkdown;
--- index.js
+++ index.js
@@ -1,75 +1,83 @@
 // TODO: Include packages needed for this application
 import fs, { read, writeFile } from 'fs'
 import inquirer from 'inquirer'
-import generateMarkdown from './generateMarkdown'
+import generateMarkdown from './generateMarkdown.js'
 
 // TODO: Create an array of questions for user input
-// const readmeQuestions = [];
-const readmePrompt = () => {
+const readmeQuestions = [
 
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'projectName'
-        },
-        {
-            type: 'input',
-            message: 'What is the what, why, and how description of your project?',
-            name: 'projectDescription'
-        },
-        {
-            type: 'input',
-            message: 'What are the the step-by-step instructions on how someone can install your project?',
-            name: 'installationInstructions'
-        },
-        {
-            type: 'input',
-            message: 'How can someone else use your project?',
-            name: 'usageInstructions'
-        },
-        {
-            type: 'input',
-            message: 'What resources were used or who contributed to the project?',
-            name: 'contributions'
-        },
-        {
-            type: 'input',
-            message: 'What tests, commands, or resources were used to test your project?',
-            name: 'testInstructions'
-        },
-        {
-            type: 'list',
-            message: 'Which license was used on your project?',
-            name: 'licenseInformation',
-            choices: ['MIT', 'GNU GPLv3', 'Rails', 'Apache License 2.0', 'ISC']
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub name?',
-            name: 'gitHub'
-        },
-        {
-            type: 'input',
-            message: 'What email address can you be reached at?',
-            name: 'emailAddress'
-        },
-    ])
-}
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectName'
+    },
+    {
+        type: 'input',
+        message: 'What is the what, why, and how description of your project?',
+        name: 'projectDescription'
+    },
+    {
+        type: 'input',
+        message: 'What are the the step-by-step instructions on how someone can install your project?',
+        name: 'installationInstructions'
+    },
+    {
+        type: 'input',
+        message: 'How can someone else use your project?',
+        name: 'usageInstructions'
+    },
+    {
+        type: 'input',
+        message: 'What resources were used or who contributed to the project?',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'What tests, commands, or resources were used to test your project?',
+        name: 'testInstructions'
+    },
+    {
+        type: 'list',
+        message: 'Which license was used on your project?',
+        name: 'licenseInformation',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'ISC', 'none']
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub name?',
+        name: 'gitHub'
+    },
+    {
+        type: 'input',
+        message: 'What email address can you be reached at?',
+        name: 'emailAddress'
+    },
+
+]
 
 
 // TODO: Create a function to write README file
 
 // .then((answers) => console.log(answers))
 
-const init = () => {
-    readmePrompt()
+// `${answers.projectName}.md`
 
-        .then((answers)) => writeFile(`${answers.projectName}.md`, generateMarkdown(answers))
-            .then(() => console.log('Successfully wrote files to '`${answers.projectName}.md`))
-            .catch((err) => console.error(err));
-};
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      console.log(err ? `Error writing file: ${err}` : 'Successfully wrote files to README.md !');
+    });
+  }
+
+
 // TODO: Create a function to initialize app
+
+function init() {
+    inquirer.prompt(readmeQuestions).then((answers) => {
+      const data = generateMarkdown(answers);
+      writeToFile('README.md', data);
+    });
+  }
+
 // function init() {
 //     prompt
 // }
